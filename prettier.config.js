d183d1f31d3709bd5a6efece1dbf00609a4f04d8
Aitugan <mirash2001@gmail.com>
2020-10-12 23:40:32 +0500

    Initial commit. Configured Prettier formatter

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 90,
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    jsxBracketSameLine: false,
+}
