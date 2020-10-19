d2cf6c90250c160047cea36615eadc9c6dfd13e6
Aitugan <mirash2001@gmail.com>
2020-10-19 17:14:24 +0500

    Call for vacancies api to get list of vacancies

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,18 @@
 <script>
 import MainSearch from "@/components/Main/MainSearch";
 export default {
-  components: {MainSearch}
+  components: {MainSearch},
+  async asyncData({$axios}) {
+    try {
+      let response = await $axios.$get("/api/vacancies")
+      return {
+        vacancies:response.vacancies // Use to list over vacancies
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 }
 </script>
 <style>
