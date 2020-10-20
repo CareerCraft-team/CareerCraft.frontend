89eb63a7901a760e3af45e746489fa9ab79d8ee9
Aitugan <mirash2001@gmail.com>
2020-10-20 23:57:35 +0500

    Calls to get vacancy and comments on it

---
+++ pages/vacancies/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios }) {
+        try {
+        let singleVacancy = $axios.$get(`/api/vacancies/${params.id}`);
+        let manyComments = $axios.$get(`/api/comments/${params.id}`);
+
+        const [vacancyResponse, commentResponse] = await Promise.all([
+            singleVacancy,
+            manyComments
+        ]);
+
+        return {
+            vacancy: vacancyResponse.vacancy,
+            reviews: commentResponse.comments
+        };
+        } catch (err) {
+            console.log(err);
+        }
+  },
+}
+</script>
+
+<style>
+
+</style>
